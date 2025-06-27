fc23224646a82a68afa1f973f7d7710f6952efaf
Igor Lezhnin <i.lezhnin@redmadrobot.com>
2025-06-27 22:56:34 +0700

    Add metrics

--- backend/src/services/sendMail.js
+++ backend/src/services/sendMail.js
@@ -2,9 +2,20 @@ const Nodemailer = require('nodemailer');
 const { MailtrapTransport } = require('mailtrap');
 const circuitBreaker = require('@/utils/emailCircuitBreaker');
 const logger = require("@/utils/logger");
+const {metrics} = require("@/utils/emailMetrics");
+const {sleep} = require("@/utils/sleep");
+const {randomIntFromInterval} = require("@/utils/randomIntFromInterval");
 
 const sendMailImpl = async ({ email, name, subject, link, type, htmlContent }) => {
+    let endTimer;
+
+    // [METRICS] Incrementing request in progress
+    metrics.emailInProgress.inc(1);
+
     try {
+        // [METRICS] Starting timer
+        endTimer = metrics.emailDuration.startTimer();
+
         const transport = Nodemailer.createTransport(
             MailtrapTransport({
                 token: process.env.MAILTRAP_TOKEN || 'aee38359c7eb0868230308c65d872439',
@@ -25,18 +36,54 @@ const sendMailImpl = async ({ email, name, subject, link, type, htmlContent }) =
             category: type,
         });
 
-        const info = await transport.sendMail({
-            from: sender,
-            to: email,
-            subject,
-            html: htmlContent,
-            category: type,
-        });
+        // TODO:
+        // const info = await transport.sendMail({
+        //     from: sender,
+        //     to: email,
+        //     subject,
+        //     html: htmlContent,
+        //     category: type,
+        // });
+
+        const info = {
+            some_mock_data: 'MOCK DATA',
+            random: Math.random(),
+        }
+
+        await sleep(randomIntFromInterval(500, 2000));
 
         logger.info('[sendMail.js] Invoice mail has been successfully sent');
         logger.debug('[sendMail.js] Invoice mail has been successfully sent', {
             info,
         });
+
+        // [METRICS] Incrementing counter
+        metrics.emailCounter.inc(1);
+
+        const requestSize = Buffer.byteLength(JSON.stringify({
+            from: sender,
+            to: email,
+            subject,
+            html: htmlContent,
+            category: type,
+        }));
+
+        // [METRICS] Saving request size
+        metrics.emailRequestSize.observe(requestSize);
+
+        const responseSize = Buffer.byteLength(JSON.stringify(info));
+        // [METRICS] Saving response size
+        metrics.emailResponseSize.observe(responseSize);
+
+        // [METRICS] Finishing timer
+        endTimer();
+
+        // [METRICS] Decreasing request in progress
+        metrics.emailInProgress.dec(1);
+
+        metrics.emailBusinessMetric.inc({
+            outcome: 'success'
+        })
 
         return {
             message_ids: [info.messageId],
@@ -50,6 +97,22 @@ const sendMailImpl = async ({ email, name, subject, link, type, htmlContent }) =
     } catch (error) {
         logger.error('[sendMail.js] Invoice mail has not been sent', {
             error,
+        })
+
+        // [METRICS] Finishing timer
+        endTimer();
+
+        // [METRICS] Decreasing request in progress
+        metrics.emailInProgress.dec(1);
+
+        metrics.emailBusinessMetric.inc({
+            outcome: 'fail'
+        })
+
+        metrics.emailErrors.inc({
+            labels: {
+                error_type: error.name || 'UnknownError',
+            }
         })
 
         throw new Error('Failed to send email: ' + error.message);
